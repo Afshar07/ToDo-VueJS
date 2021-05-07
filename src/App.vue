65adcbdce133377febf33cd5c6abb763e26ce489
Afshar07 <mr.afshar0@gmail.com>
2021-05-07 22:53:50 +0430

    Add TheHeader to the app

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,15 @@
 <template>
+<the-header title="Todo List"></the-header>
   <stored-resources :resources="storedResources"></stored-resources>
 </template>
 
 <script>
 import StoredResources from './components/learning-resources/StoredResources.vue';
+import TheHeader from './components/Layouts/TheHeader.vue';
 export default {
   components: {
     StoredResources,
+    TheHeader,
   },
   data() {
     return {
