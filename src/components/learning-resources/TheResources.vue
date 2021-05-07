642731c0004c113348e12cd01818d9d44b349fd4
Afshar07 <mr.afshar0@gmail.com>
2021-05-07 23:21:30 +0430

    Create a new component for displaying stored resources/Add resource

---
+++ src/components/learning-resources/TheResources.vue
@@ -0,0 +1,52 @@
+<template>
+  <base-card>
+    <base-button @click="setSelectedTab('stored-resources')"
+      >Stored Resources</base-button
+    >
+    <base-button @click="setSelectedTab('add-resource')"
+      >Add Resource</base-button
+    >
+  </base-card>
+
+  <component :is="selectedTab"></component>
+</template>
+
+<script>
+import StoredResources from './StoredResources.vue';
+import AddResource from './AddResource.vue';
+export default {
+  components: {
+    StoredResources,
+    AddResource
+  },
+  data() {
+    return {
+      selectedTab: 'stored-resources',
+      storedResources: [
+        {
+          id: 'official-guide',
+          title: 'Official Guide',
+          description: 'The Official VueJS documentation!',
+          link: 'https://www.vuejs.org'
+        },
+        {
+          id: 'google',
+          title: 'Google',
+          description: 'Learn how to google stuff!',
+          link: 'https://www.google.com'
+        }
+      ]
+    };
+  },
+  methods: {
+    setSelectedTab(tab) {
+      this.selectedTab = tab;
+    }
+  },
+  provide() {
+    return {
+      resources: this.storedResources
+    };
+  }
+};
+</script>
