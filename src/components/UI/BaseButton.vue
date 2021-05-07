393263272fa15f73d7dfb776d0d915961548259c
Afshar07 <mr.afshar0@gmail.com>
2021-05-07 23:01:45 +0430

    Add the BaseButton component and styling instead of button tag

---
+++ src/components/UI/BaseButton.vue
@@ -0,0 +1,26 @@
+<template>
+  <button :type="type" :class="mode">
+      <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  props: ['type', 'mode']
+};
+</script>
+
+<style scoped>
+.delete {
+  border: none;
+  width: 6vw;
+  height: 4vh;
+  background-color: rgba(160, 0, 0, 0.84);
+  color: #fff;
+  cursor: pointer;
+  border-radius: 30px;
+}
+.delete:hover {
+  background-color: rgb(160, 0, 0);
+}
+</style>
